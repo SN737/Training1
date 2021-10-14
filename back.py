e52ec0120f1b845b6bcab2323968347fe61f53ba
Sergey-737 <111@111.ru>
2021-10-14 09:58:35 +0300

    интересная задачка без IF

---
+++ back.py
@@ -0,0 +1,12 @@
+
+n=int(input())
+k = n//4+1
+print(k)
+
+
+a=int(input())
+b=int(input())
+s=(a+b)**2
+q= a**2+b**2
+print("Квадрат суммы", a, "и", b, "равен", s)
+print("Сумма квадратов", a, "и", b, "равна", q)
