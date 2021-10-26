60d4ef2352dc27f7c90450bec7e6650856ef99d0
Sergey-737 <111@111.ru>
2021-10-26 12:26:37 +0300

    Всем известно, что ведьмак способен одолеть любых чудовищ, однако его услуги обойдутся недешево, к тому же ведьмак не принимает купюры, он принимает только чеканные монеты. В мире ведьмака существуют монеты с номиналами 1, 5,  10, 25. + моя модификация с подсчётом сколько нужно монет каждого номинала

--- block_4.py
+++ block_4.py
@@ -1,21 +1,29 @@
-a = input()
-b = input()
+# from math import *
 
-if a == "желтый" or a == "синий" or a == "красный":
-    if b == "желтый" or b == "синий" or b == "красный":
-        if a == b:
-            print(a)
-        elif a == "желтый" and b == "красный" or b == "желтый" and a == "красный":
-            print("оранжевый")
-        elif a == "желтый" and b == "синий" or b == "желтый" and a == "синий":
-            print("зеленый")
-        elif a == "красный" and b == "синий" or b == "красный" and a == "синий":
-            print("фиолетовый")
-    else:
-        print("ошибка цвета")
-else:
-    print("ошибка цвета")
+a = int(input())
+total  = 0
+M25 = 0
+M10 = 0
+M5 = 0
+M1 = 0
 
+while a > 0:
+    while  a // 25 > 0:
+        M25 += 1
+        a = a - 25
+    print ("M25 -", M25)
+    while a // 10 > 0:
+        M10 += 1
+        a = a - 10
+    print("M10 -", M10)
+    while a // 5 > 0:
+        M5 += 1
+        a = a - 5
+    print("M5 -", M5)
+    while a // 1 > 0:
+        M1 += 1
+        a = a - 1
+    print("M1 -", M1)
 
-
-
+total = M25 + M10 + M5 + M1
+print("Всего монет:", total)
